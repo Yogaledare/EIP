b34f242441cb26fb42176ed05888766333fe50e6
Yogaledare <marcus_uppstrom@hotmail.com>
2024-03-06 15:34:45 +0100

    initial commit

---
+++ EIP/Person.cs
@@ -0,0 +1,46 @@
+﻿namespace EIP;
+
+class Person {
+    private int age = 5;
+    private string fName;
+    private string lName;
+    private int height;
+    private int weight;
+
+    public int Age {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public string FName {
+        get { return fName; }
+        set { fName = value; }
+    }
+    
+    public string LName {
+        get { return lName; }
+        set { lName = value; }
+    }
+    
+    public int Height {
+        get { return height; }
+        set { height = value; }
+    }
+
+    public int Weight {
+        get { return weight; }
+        set { weight = value; }
+    }
+
+
+
+    public void Validate() {
+        
+        
+        // validation logic
+    }
+    
+        
+        
+        
+}
---
+++ EIP/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EIP;
+
+class Program {
+    static void Main(string[] args) {
+
+
+
+        var person = new Person();
+
+        var age = person.Age;
+
+        Console.WriteLine(age);
+
+        var fName = person.FName;
+
+        Console.WriteLine(fName);
+    }
+
+
+   
+    
+    
+    
+    
+}
